1d2064f79097d1795f80a01353b79f7161e44680
Dustin Fedie <dlfedie@gmail.com>
2019-07-08 11:25:15 -0500

    new function to compare rank, ace is still low, though

--- objectsPractical.js
+++ objectsPractical.js
@@ -8,6 +8,12 @@ let card1 = {
   number: 1
 } //end card1
 
+let card2 = {
+  suit: 'Diamonds',
+  number: 1
+} //end card1
+
+
 function isRed(cardToCheck) {
   console.log('In isRed:', cardToCheck);
   if (cardToCheck.suit === 'Hearts' || cardToCheck.suit === 'Diamonds') {
@@ -22,5 +28,26 @@ function isRed(cardToCheck) {
 
 console.log(isRed(card0));
 console.log(isRed(card1));
+console.log(isRed(card2));
 
 //write a function that receives 2 cards, returns the higher one
+
+function isHigher(firstCard, secondCard) {
+  console.log('In isHigher:', firstCard, secondCard);
+  if (firstCard.number > secondCard.number) {
+    console.log(firstCard, 'is the higher card');
+    return firstCard;
+  } // first higher
+  else if (firstCard.number < secondCard.number) {
+    console.log(secondCard, 'is the higher card');
+    return secondCard;
+  } // second higher
+  else {
+    console.log('Both cards have the same number!', firstCard, secondCard);
+    return firstCard;
+  } // equal
+} //end isHigher
+
+console.log(isHigher(card0, card2));
+console.log(isHigher(card2, card0));
+console.log(isHigher(card1, card2));
