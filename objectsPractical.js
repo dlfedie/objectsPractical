b47bf8443dbed7c44875a171a6e21221ac3eb301
Dustin Fedie <dlfedie@gmail.com>
2019-07-10 15:09:35 -0500

    More testing

--- objectsPractical.js
+++ objectsPractical.js
@@ -70,3 +70,16 @@ function isHigher(firstCard, secondCard) {
 console.log(isHigher(card0, card2));
 console.log(isHigher(card2, card0));
 console.log(isHigher(card1, card2));
+
+
+///
+
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+let myFather = new Person("John", "Doe", 50, "blue");
+console.log(myFather);
